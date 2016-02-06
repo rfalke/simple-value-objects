5c3bd2b8604922acf82aa6730b48b89fbd5f540c
Raimar Falke <raimar.falke@codecentric.de>
2016-02-06 18:03:29 +0100

    Replace tabs with spaces. Cleanup of pom.xml.

--- jackson-normal/src/main/java/de/codecentric/demo/simple/value/objects/jackson/domain/Address.java
+++ jackson-normal/src/main/java/de/codecentric/demo/simple/value/objects/jackson/domain/Address.java
@@ -4,42 +4,42 @@ import java.io.Serializable;
 
 public class Address implements Serializable {
 
-	private static final long serialVersionUID = 1373565100996488402L;
+    private static final long serialVersionUID = 1373565100996488402L;
 
-	private String street;
-	private String houseNumber;
-	private String zipCode;
-	private String city;
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+    private String city;
 
-	public String getStreet() {
-		return street;
-	}
+    public String getStreet() {
+        return street;
+    }
 
-	public void setStreet(final String street) {
-		this.street = street;
-	}
+    public void setStreet(final String street) {
+        this.street = street;
+    }
 
-	public String getHouseNumber() {
-		return houseNumber;
-	}
+    public String getHouseNumber() {
+        return houseNumber;
+    }
 
-	public void setHouseNumber(final String houseNumber) {
-		this.houseNumber = houseNumber;
-	}
+    public void setHouseNumber(final String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
 
-	public String getZipCode() {
-		return zipCode;
-	}
+    public String getZipCode() {
+        return zipCode;
+    }
 
-	public void setZipCode(final String zipCode) {
-		this.zipCode = zipCode;
-	}
+    public void setZipCode(final String zipCode) {
+        this.zipCode = zipCode;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setCity(final String city) {
-		this.city = city;
-	}
+    public void setCity(final String city) {
+        this.city = city;
+    }
 }
--- jackson-normal/src/main/java/de/codecentric/demo/simple/value/objects/jackson/domain/Company.java
+++ jackson-normal/src/main/java/de/codecentric/demo/simple/value/objects/jackson/domain/Company.java
@@ -6,38 +6,38 @@ import java.util.List;
 
 public class Company implements Serializable {
 
-	private static final long serialVersionUID = -7531923832214468716L;
+    private static final long serialVersionUID = -7531923832214468716L;
 
-	private String name;
-	private Address companyAddress;
-	private List<Customer> customers;
+    private String name;
+    private Address companyAddress;
+    private List<Customer> customers;
 
-	// jackson required a default constructor for deserializen
-	public Company() {
-		this.customers = new ArrayList<Customer>();
-	}
+    // jackson required a default constructor for deserializen
+    public Company() {
+        this.customers = new ArrayList<Customer>();
+    }
 
-	public final String getName() {
-		return name;
-	}
+    public final String getName() {
+        return name;
+    }
 
-	public final void setName(final String name) {
-		this.name = name;
-	}
+    public final void setName(final String name) {
+        this.name = name;
+    }
 
-	public final Address getCompanyAddress() {
-		return companyAddress;
-	}
+    public final Address getCompanyAddress() {
+        return companyAddress;
+    }
 
-	public final void setCompanyAddress(final Address companyAddress) {
-		this.companyAddress = companyAddress;
-	}
+    public final void setCompanyAddress(final Address companyAddress) {
+        this.companyAddress = companyAddress;
+    }
 
-	public final List<Customer> getCustomers() {
-		return customers;
-	}
+    public final List<Customer> getCustomers() {
+        return customers;
+    }
 
-	public final void setCustomers(final List<Customer> customers) {
-		this.customers = customers;
-	}
+    public final void setCustomers(final List<Customer> customers) {
+        this.customers = customers;
+    }
 }
