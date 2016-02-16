45d8d0cfee1f99d2fedb2a0cf42c6f2e625f93a3
Sebastian Nemak <s7nio@users.noreply.github.com>
2016-02-16 17:32:06 +0100

    Update test for hibernate-public-fields (persist, select, update)

--- hibernate-public-fields/src/test/java/de/codecentric/demo/simple/value/objects/hibernate/HibernatePublicFieldsTest.java
+++ hibernate-public-fields/src/test/java/de/codecentric/demo/simple/value/objects/hibernate/HibernatePublicFieldsTest.java
@@ -10,10 +10,9 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import de.codecentric.demo.simple.value.objects.hibernate.entities.Address;
@@ -22,52 +21,71 @@ import de.codecentric.demo.simple.value.objects.hibernate.entities.Customer;
 
 public class HibernatePublicFieldsTest {
 
-    @Test
-    public void testPersist() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("simple-value-objects");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        assertNotNull(entityManager);
+	@Test
+	public void testPersist() {
+		EntityManager entityManager = getEntityManager();
 
-        entityManager.getTransaction().begin();
-        Company company = createCompany();
-        company = entityManager.merge(company);
-        entityManager.getTransaction().commit();
+		save(entityManager, createCompany());
 
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Company> cq = cb.createQuery(Company.class);
-        CriteriaQuery<Company> select = cq.select(cq.from(Company.class));
-        TypedQuery<Company> q = entityManager.createQuery(select);
-        List<Company> companies = q.getResultList();
+		List<Company> selectedCompanies = select(entityManager);
+		Assert.assertEquals(1, selectedCompanies.size());
 
-        assertEquals("Example Inc.", companies.get(0).name);
-    }
+		update(entityManager, selectedCompanies.get(0));
+		assertEquals("Sample Inc.", select(entityManager).get(0).name);
+	}
 
-    private Company createCompany() {
-        Address companyAddress = new Address();
-        companyAddress.street = "Ritterstr.";
-        companyAddress.houseNumber = "11";
-        companyAddress.zipCode = "10969";
-        companyAddress.city = "Berlin";
-        Address generalCustomerAddress = new Address();
-        generalCustomerAddress.street = "Friedrichstraße";
-        generalCustomerAddress.houseNumber = "43-45";
-        generalCustomerAddress.zipCode = "10117";
-        generalCustomerAddress.city = "Berlin";
+	private void save(final EntityManager em, final Company company) {
+		em.getTransaction().begin();
+		em.persist(company);
+		em.getTransaction().commit();
+	}
 
-        Company company = new Company();
-        company.name = "Example Inc.";
-        company.companyAddress = companyAddress;
+	private List<Company> select(final EntityManager em) {
+		CriteriaQuery<Company> cq = em.getCriteriaBuilder().createQuery(Company.class);
+		CriteriaQuery<Company> select = cq.select(cq.from(Company.class));
+		return em.createQuery(select).getResultList();
+	}
 
-        List<Customer> customers = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            Customer customer = new Customer();
-            customer.firstName = UUID.randomUUID().toString();
-            customer.lastName = UUID.randomUUID().toString();
-            customer.customerAddress = generalCustomerAddress;
-            customer.company = company;
-            customers.add(customer);
-        }
-        company.customers = customers;
-        return company;
-    }
+	private void update(final EntityManager em, final Company company) {
+		em.getTransaction().begin();
+		company.name = "Sample Inc.";
+		em.merge(company);
+		em.getTransaction().commit();
+	}
+
+	private EntityManager getEntityManager() {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("simple-value-objects");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		assertNotNull(entityManager);
+		return entityManager;
+	}
+
+	private Company createCompany() {
+		Address companyAddress = new Address();
+		companyAddress.street = "Ritterstr.";
+		companyAddress.houseNumber = "11";
+		companyAddress.zipCode = "10969";
+		companyAddress.city = "Berlin";
+		Address generalCustomerAddress = new Address();
+		generalCustomerAddress.street = "Friedrichstraße";
+		generalCustomerAddress.houseNumber = "43-45";
+		generalCustomerAddress.zipCode = "10117";
+		generalCustomerAddress.city = "Berlin";
+
+		Company company = new Company();
+		company.name = "Example Inc.";
+		company.companyAddress = companyAddress;
+
+		List<Customer> customers = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			Customer customer = new Customer();
+			customer.firstName = UUID.randomUUID().toString();
+			customer.lastName = UUID.randomUUID().toString();
+			customer.customerAddress = generalCustomerAddress;
+			customer.company = company;
+			customers.add(customer);
+		}
+		company.customers = customers;
+		return company;
+	}
 }
